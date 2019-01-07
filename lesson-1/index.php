e8286dd6cdf3dcc582542ed0d896a6c2ce16ad8f
Kurchenkov <43772006+Kurchenkov@users.noreply.github.com>
2019-01-07 11:16:32 +0300

    Update index.php

--- lesson-1/index.php
+++ lesson-1/index.php
@@ -1,38 +1,4 @@
-<?php
 /*1. Придумать класс, который описывает любую сущность из предметной области интернет-магазинов: продукт, ценник, посылка и т.п.
 2. Описать свойства класса из п.1 (состояние).
 3. Описать поведение класса из п.1 (методы).
 4. Придумать наследников класса из п.1. Чем они будут отличаться?*/
-
-   class Good {
-        private $name;
-        private $price;  
-        public function __construct($name, $price) {
-            $this->name = $name;
-            $this->price = $price;
-        }
-        public function getName() {
-            return $this->name;
-        }
-        public function getPrice() {
-            return $this->price;
-        }
-    }
-
-    class GoodUnit extends Good {
-        private $color;
-        public function __construct($name, $price, $color) {
-            parent::__construct($name, $price);
-            $this->color = $color;
-        }
-        public function getColor() {
-            return $this->color;
-        }
-        public function showGoodProps() {
-            echo 'name: '.$this->getName().'<br/> price: '.$this->getPrice().'<br/> color: '.$this->getColor();
-        }
-    }
-
-    $object = new GoodUnit("carpet", 1000, "brown");
-    $object->showGoodProps();
-?>
